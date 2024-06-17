31fa3112a70c53ce6020e9306a0bfd3040590141
cazuho <cakumubom9210@gmail.com>
2024-06-17 12:07:33 +0900

    feat(chap15): ログイン、ログアウト機能を追加
    メールとパスワードを使用した認証ロジックを作成し、ログインフォームと合わせてログイン処理を追加しました。
    また、サインアウトボタンを押すとログアウトできるようにしました。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -126,5 +128,25 @@ export async function deleteInvoice(id: string) {
         return { message: 'Deleted Invoice.' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+}
+
+// ログインフォームの入力をsingIn関数に渡す
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
