80694e8383272402481cbccae6b2a047356d21d5
cazuho <cakumubom9210@gmail.com>
2024-06-17 11:01:03 +0900

    feat(chap14): アクセシビリティの向上
    サーバー上でフォーム入力を検証し、各フィールドが正しく入力されているかをチェックするようにしました。エラーがある場合はフォームにエラーメッセージを表示します。
    また、アクセシビリティを向上させるためにlintツールを導入し、コードを検証しました。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,9 +8,15 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
@@ -18,13 +24,35 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
     // フォームに入力したデータを取得し、型を検証して変数に格納
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    /// console.log(validatedFields);
+    // 空のフォームを送信すると、このような情報が得られた { success: false, error: [Getter] }
+
+    // 情報をデータベースに送信する前に検証し、失敗した場合エラーメッセージを返す
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -49,13 +77,25 @@ export async function createInvoice(formData: FormData) {
 }
 
 // 作成と同様にフォームデータの取得、検証、データベースへの更新を行う
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
